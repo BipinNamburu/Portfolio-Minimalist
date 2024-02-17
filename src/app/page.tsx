12902b8c904ae3bf6ace34c009d7ae8ed7a6854f
BipinNamburu <bipinnamburu2244@gmail.com>
2024-02-17 17:29:39 +0530

    added comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
   description: RESUME_DATA.summary,
 };
-
+//checking
 export default function Page() {
   return (
     <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
